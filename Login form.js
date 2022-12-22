4aeb294d50f0a29f4a2300ec2319ce3588eb1edb
Pallob Saha <121215608+Pallobsaha48@users.noreply.github.com>
2022-12-22 19:58:46 +0600

    Create Login form.js

---
+++ Login form.js
@@ -0,0 +1,11 @@
+var attempt = 3; //Variable to count number of attempts
+............
+attempt --; //Decrementing by one
+.............
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
